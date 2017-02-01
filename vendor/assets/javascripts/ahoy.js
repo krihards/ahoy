93f8a6284b135e98e258056fbd5aa2c77162134e
Rihards <krihards@me.com>
2017-02-01 14:10:18 +0200

    added withCredentials = true to ahoy.jos ajax post

--- vendor/assets/javascripts/ahoy.js
+++ vendor/assets/javascripts/ahoy.js
@@ -151,6 +151,9 @@
       $.ajax({
         type: "POST",
         url: url,
+        xhrFields: { 
+          withCredentials: true 
+        },
         data: JSON.stringify(data),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
